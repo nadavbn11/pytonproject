3f8eb03274f52f941eade38597eeea41c76e547a
nadavgot <nadavbn11@gmail.com>
2023-06-24 12:52:35 +0300

    ddd

---
+++ PycharmProjects/pythonProject/main.py
@@ -0,0 +1,7 @@
+
+
+
+#12.Write a program with the following:
+#. Method that gets a number from the user (using input).
+#. Method that receives the number from the first method, and computes the sum of
+#the digits the integer (e.g. 25 = 7, 2+5=7)
